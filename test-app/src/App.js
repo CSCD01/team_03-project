83ddf47c1f098b8695dd651fdf02de1e320c302a
KostyaIliouk <kostya.iliouk@hotmail.com>
2020-04-06 19:20:38 -0400

    acceptance test complete

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -46,67 +46,6 @@ function App() {
             4
           </ListGroup.Item>
       </ListGroup>
-      {/* <div>
-      <Tab.Container id="left-tabs-example" defaultActiveKey="first">
-        <Row>
-          <Col sm={3}>
-            <Nav variant="pills" className="flex-column">
-              <Nav.Item>
-                <Nav.Link eventKey="first">Tab 1</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="second">Tab 2</Nav.Link>
-              </Nav.Item>
-            </Nav>
-          </Col>
-          <Col sm={9}>
-            <Tab.Content>
-              <Tab.Pane eventKey="first" unmountOnExit={true}>
-                <p> TEST ONE</p>
-              </Tab.Pane>
-              <Tab.Pane eventKey="second" unmountOnExit={false}>
-              <p> TEST TWO</p>
-              </Tab.Pane>
-            </Tab.Content>
-          </Col>
-        </Row>
-      </Tab.Container>
-      </div>
-      <br/>
-      <Carousel>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            alt="Second slide"
-          />
-
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            alt="Third slide"
-          />
-
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel> */}
     </div>
   );
 }
